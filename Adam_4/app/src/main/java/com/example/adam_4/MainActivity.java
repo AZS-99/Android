c803aa621eae818993af59d097eeb79156e2fe66
Adam Saher <sadam4@myseneca.ca>
2019-10-16 13:58:41 -0400

    Renaming activities

--- Adam_4/app/src/main/java/com/example/adam_4/MainActivity.java
+++ Adam_4/app/src/main/java/com/example/adam_4/MainActivity.java
@@ -5,7 +5,6 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
@@ -23,14 +22,14 @@ public class MainActivity extends AppCompatActivity {
 
 
         listView = (ListView)findViewById(R.id.simpleListView);
-        arrayAdapter = new ArrayAdapter<String>(this, R.layout.activity_alpha_list_view, R.id.item, letters);
+        arrayAdapter = new ArrayAdapter<String>(this, R.layout.activity_alpha_list_view_cell, R.id.item, letters);
         listView.setAdapter(arrayAdapter);
 
         numbers = new ArrayList<>();
         for (int i = 5; i < 500; i+=5)
             numbers.add(Integer.toString(i));
         gridView = (GridView)findViewById(R.id.simpleGridView);
-        arrayAdapter = new ArrayAdapter<String>(this, R.layout.activity_numeric_grid, R.id.numericItem, numbers);
+        arrayAdapter = new ArrayAdapter<String>(this, R.layout.activity_numeric_grid_cell, R.id.numericItem, numbers);
         gridView.setAdapter(arrayAdapter);
     }
 }
